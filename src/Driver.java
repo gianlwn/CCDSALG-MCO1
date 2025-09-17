2a1c5ed26ef41e3e09d5a05e6605a060b725a08c
Giancarlo Lawan <lawangiancarlo10@gmail.com>
2025-09-17 18:59:55 +0800

    Added contents for Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Driver {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String inputStr = sc.nextLine();
+
+        int size = inputStr.length();
+        //System.out.println(n);
+
+        String[] suffixArr = new String[size];
+
+        for (int i = 0; i < size; i++) {
+            suffixArr[i] = inputStr.substring(i, size);
+        }
+
+        int width = String.valueOf(size - 1).length();
+        for (int i = 0; i < size; i++) {
+            System.out.printf("%" + width + "d: %-15s%n", i, suffixArr[i]);
+        }
+            
+        sc.close();
     }
 }
