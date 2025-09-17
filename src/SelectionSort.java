ef0465b1e975fa925d0269d57b13f3af8faa796b
gianlwn <lawangiancarlo10@gmail.com>
2025-09-17 19:18:42 +0800

    implemented the algorithm

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,15 +1,22 @@
 public class SelectionSort {
-    public static String selectionSort(String[] suffixArr, int size) {
+    public static void swap(String[] sfxArr, int i, int minIdx) {
+        String temp = String sfxArr[i];
+        sfxArr[i] = sfxArr[minIdx];
+        sfxArr[minIdx] = temp;
+    }
+    
+    public static String selectionSort(String[] sfxArr, int size) {
         for (int i = 0; i < size - 1; i++) {
             int minIdx = i;
 
             for (int j = i + 1; j < size; j++) {
-                if (suffixArr[j].compareTo(suffixArr[minIdx]) < 0) {
+                if (sfxArr[j].compareTo(sfxArr[minIdx]) < 0) {
                     minIdx = j;
                 }
             }
 
             // swap
+            swap(sfxArr, i, minIdx);
         }
 
         return "";
