8fcd7bfaf952e603f952b6a765bd341a1441def0
Giancarlo Lawan <lawangiancarlo10@gmail.com>
2025-09-18 18:04:50 +0800

    Selection sort algorithm is done, we need to implement quick sort now.

---
+++ src/DriverQuickSort.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class DriverQuickSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String inputStr = sc.nextLine();
+        int size = inputStr.length();
+
+        String[] unsortedSfx = new String[size];
+        String[] sortedSfx = new String[size];
+        int [] sfxArr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            unsortedSfx[i] = inputStr.substring(i, size);
+            sfxArr[i] = i;
+        }
+
+        int width = String.valueOf(size - 1).length();
+        System.out.println("\nAll suffixes: \n");
+        for (int i = 0; i < size; i++)
+            System.out.printf("%" + width + "d: %-15s%n", sfxArr[i], unsortedSfx[i]);
+
+        SelectionSort.selectionSort(unsortedSfx, sortedSfx, sfxArr, size);
+
+        System.out.println("\nSorted suffixes: \n");
+        for (int i = 0; i < size; i++)
+            System.out.printf("%" + width + "d: %-15s%n", sfxArr[i], sortedSfx[i]);
+
+        System.out.println("\nSuffix array: \n");
+        for (int i = 0; i < size; i++)
+            if (i == size - 1)
+                System.out.print(sfxArr[i]);
+            else
+                System.out.print(sfxArr[i] + ", ");
+            
+        sc.close();
+    }
+}
--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,26 +1,27 @@
 public class SelectionSort {
-    public static void swap(String[] unsortedStrings, int[] sfxArr, int i, int minIdx) {
-        String temp = unsortedStrings[i];
-        unsortedStrings[i] = unsortedStrings[minIdx];
-        unsortedStrings[minIdx] = temp;
+    public static void swap(String[] unsortedSfx, int[] sfxArr, int i, int minIdx) {
+        String temp = unsortedSfx[i];
+        unsortedSfx[i] = unsortedSfx[minIdx];
+        unsortedSfx[minIdx] = temp;
 
         int nTemp = sfxArr[i];
         sfxArr[i] = sfxArr[minIdx];
         sfxArr[minIdx] = nTemp;
     }
     
-    public static void selectionSort(String[] unsortedStrings, int[] sfxArr, int size) {
+    public static void selectionSort(String[] unsortedSfx, String[] sortedSfx, int[] sfxArr, int size) {
         for (int i = 0; i < size - 1; i++) {
             int minIdx = i;
 
-            for (int j = i + 1; j < size; j++) {
-                if (unsortedStrings[j].compareTo(unsortedStrings[minIdx]) < 0) {
+            for (int j = i + 1; j < size; j++)
+                if (unsortedSfx[j].compareTo(unsortedSfx[minIdx]) < 0)
                     minIdx = j;
-                }
-            }
 
             // swap
-            swap(unsortedStrings, sfxArr, i, minIdx);
+            swap(unsortedSfx, sfxArr, i, minIdx);
         }
+
+        for (int i = 0; i < size; i++)
+            sortedSfx[i] = unsortedSfx[i];
     }
 }
